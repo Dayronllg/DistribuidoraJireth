18211da0655527314248e839cd4670a7585cef6a
MaximusL9 <Maximusmendezgonzalez@gmail.com>
2025-05-13 02:56:33 -0600

    Formulario HTML y CSS

--- Frontend/React-ts/src/App.tsx
+++ Frontend/React-ts/src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="formulario">
+        <h1>Inicio de Sesión</h1>
+        <form method="post">
+          {/**Input del nombre del Usuario */}
+          <div className="nombreUsuario">
+            <input type="text" required></input>
+            <label>Nombre de Usuario</label>
+          </div>
+
+          {/** Input de la contraseña*/}
+          <div className="nombreUsuario">
+            <input type="password" required></input>
+            <label>Contraseña</label>
+          </div>
+
+          <div className="recordar">
+            <a
+              href="https://www.mayoclinic.org/es/diseases-conditions/amnesia/diagnosis-treatment/drc-20353366"
+              target="_blank"
+            >
+              ¿Olvido su contraseña?
+            </a>
+          </div>
+          {/** Boton Iniciar */}
+          <input type="submit" value="Iniciar"></input>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
