0e41f52d4c098c4e389354ab8b1f6bb5a379863d
MaximusL9 <Maximusmendezgonzalez@gmail.com>
2025-05-18 21:34:42 -0600

    MUI Framework

--- Frontend/React-ts/src/App.tsx
+++ Frontend/React-ts/src/App.tsx
@@ -5,7 +5,7 @@ import axios from "axios";
 function App() {
   const [formData, setFormData] = useState({
     nombreUsuario: "",
-    contrasena: ""
+    contrasena: "",
   });
   const [message, setMessage] = useState("");
 
@@ -18,7 +18,10 @@ function App() {
     setMessage("Iniciando sesión...");
 
     try {
-      const response = await axios.post("http://localhost:5187/api/Auth/Login", formData);
+      const response = await axios.post(
+        "http://localhost:5187/api/Auth/Login",
+        formData
+      );
       console.log("Token recibido:", response.data.token);
       setMessage("Login Exitoso");
       // Aquí se puede guardar el token si querés: localStorage.setItem("token", response.data.token)
@@ -27,21 +30,33 @@ function App() {
       setMessage("Error al iniciar sesión");
     }
   };
-   
+
   return (
     <>
-      <div className="formulario" >
+      <div className="formulario">
         <h1>Inicio de Sesión</h1>
         <form method="post" onSubmit={handleSubmit}>
           {/**Input del nombre del Usuario */}
           <div className="nombreUsuario">
-            <input name="nombreUsuario" type="text" required onChange={handleChange} value={formData.nombreUsuario}></input>
+            <input
+              name="nombreUsuario"
+              type="text"
+              required
+              onChange={handleChange}
+              value={formData.nombreUsuario}
+            ></input>
             <label>Nombre de Usuario</label>
           </div>
 
           {/** Input de la contraseña*/}
           <div className="nombreUsuario">
-            <input name="contrasena" type="password" required onChange={handleChange} value={formData.contrasena}></input>
+            <input
+              name="contrasena"
+              type="password"
+              required
+              onChange={handleChange}
+              value={formData.contrasena}
+            ></input>
             <label>Contraseña</label>
           </div>
 
@@ -56,7 +71,7 @@ function App() {
           {/** Boton Iniciar */}
           <input type="submit" value="Iniciar"></input>
           <div className="flexdiv">
-          <a className="message-align" >{message}</a>
+            <a className="message-align">{message}</a>
           </div>
         </form>
       </div>
