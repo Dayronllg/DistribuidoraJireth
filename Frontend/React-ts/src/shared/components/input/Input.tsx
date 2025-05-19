bd7bf495809d75ce68f488afcd6a73799a01767b
MaximusL9 <Maximusmendezgonzalez@gmail.com>
2025-05-19 00:28:18 -0600

    Ordenar Carpetas

---
+++ Frontend/React-ts/src/shared/components/input/Input.tsx
@@ -0,0 +1,19 @@
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+
+interface inputProps {
+  label: string;
+}
+
+export default function BasicTextFields({ label }: inputProps) {
+  return (
+    <Box
+      component="form"
+      sx={{ "& > :not(style)": { m: 1, width: "25ch" } }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="outlined-basic" label={label} variant="outlined" />
+    </Box>
+  );
+}
